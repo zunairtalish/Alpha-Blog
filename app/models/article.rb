3df918b94a52fc526efe6d7055c665a2ba03b981
Zunair Talish <zunairtalish96@gmail.com>
2021-03-22 15:30:00 +0500

    Adding Articles Task

---
+++ app/models/article.rb
@@ -0,0 +1,4 @@
+class Article < ApplicationRecord
+  validates :title , presence: true ,length: {minimum: 5,maximum: 50}
+  validates :description ,presence: true ,length: {minimum: 5,maximum: 50}
+end
